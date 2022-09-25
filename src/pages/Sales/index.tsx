58d52daca73e7511daa92f6dda155fc53ef73294
Robson Oliveira <ropj@cin.ufpe.br>
2022-09-25 14:02:10 -0300

    feature(SalesPage): call the SalesBarChart component

--- src/pages/Sales/index.tsx
+++ src/pages/Sales/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 
-import {
-  Navbar, SalesPieChart,
-} from '../../components';
-import {
-  CardsContainer, Container, ElementsContainer,
-} from './style';
+import { Navbar, SalesPieChart, SalesBarChart } from '../../components';
+import { CardsContainer, Container, ElementsContainer } from './style';
 
-export const Sales: React.FC = () => (
-  <Container>
-    <Navbar />
-    <ElementsContainer>
-      <CardsContainer>
-        <SalesPieChart data={[28.8, 21.4, 19.7, 14.8, 7.65, 7.65]} />
-      </CardsContainer>
-    </ElementsContainer>
-  </Container>
-);
+export const SalesPage: React.FC = () => {
+  const barChartData = [
+    { category: 'remedio', value: 770 },
+    { category: 'calçado', value: 1170 },
+    { category: 'eletros', value: 820 },
+    { category: 'cozinha', value: 420 },
+    { category: 'mesa', value: 570 },
+    { category: 'banho', value: 420 },
+    { category: 'pets', value: 600 },
+    { category: 'computadores', value: 420 },
+    { category: 'celulares', value: 820 },
+    { category: 'acessorios', value: 820 },
+    { category: 'jardim', value: 1190 },
+    { category: 'praia', value: 980 },
+  ];
+
+  return (
+    <Container>
+      <Navbar />
+      <ElementsContainer>
+        <CardsContainer>
+          <SalesPieChart data={[28.8, 21.4, 19.7, 14.8, 7.65, 7.65]} />
+          <SalesBarChart data={barChartData} />
+        </CardsContainer>
+      </ElementsContainer>
+    </Container>
+  );
+};
