3d3c8873344110eb84190ee96a5291d1d178ffc6
Robson Oliveira <ropj@cin.ufpe.br>
2022-09-26 00:51:00 -0300

    style(SalesPage): update cards layout

--- src/pages/Sales/index.tsx
+++ src/pages/Sales/index.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { InfoCircleOutlined, CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 
 import {
   Navbar, SalesAreaChart, SalesPieChart, SalesBarChart,
@@ -8,6 +9,7 @@ import { CardsContainer, Container, ElementsContainer } from './style';
 import './styles.css';
 
 export const SalesPage: React.FC = () => {
+  const [selectedBarCategory, setBarCategory] = useState(0);
   const color = ['#1890FF', '#13C2C2', '#52C41A', '#FADB14', '#EB2F96', '#722ED1'];
 
   const pieCharData = [
@@ -51,6 +53,7 @@ export const SalesPage: React.FC = () => {
       color: '#722ED1', category: 'Banho', percent: 12, value: 7.65,
     },
   ];
+
   const pieCharDataMonth = [
     {
       color: '#1890FF', category: 'Remedio', percent: 9, value: 10.8,
@@ -73,19 +76,82 @@ export const SalesPage: React.FC = () => {
   ];
 
   const barChartData = [
-    { category: 'remedio', value: 770 },
-    { category: 'calçados', value: 1170 },
-    { category: 'eletros', value: 820 },
-    { category: 'cozinha', value: 420 },
-    { category: 'mesa', value: 570 },
-    { category: 'banho', value: 420 },
-    { category: 'pets', value: 600 },
-    { category: 'computadores', value: 420 },
-    { category: 'celulares', value: 820 },
-    { category: 'acessorios', value: 820 },
-    { category: 'jardim', value: 1190 },
-    { category: 'praia', value: 980 },
-
+    {
+      category: 'remedio',
+      products: [
+        { product: 'diazepan', value: 770 },
+        { product: 'dipirona', value: 1170 },
+        { product: 'mixidil', value: 820 },
+        { product: 'omeprazol', value: 570 },
+        { product: 'neosaldina', value: 372 },
+        { product: 'allegra', value: 240 },
+        { product: 'merthiolate', value: 128 },
+      ],
+    },
+    {
+      category: 'calçados',
+      products: [
+        { product: 'adidas power 123', value: 180 },
+        { product: 'sandalia havaiana verão', value: 1100 },
+        { product: 'tenis all star', value: 800 },
+        { product: 'sandalia cartago', value: 980 },
+        { product: 'tenis all jordan', value: 10 },
+        { product: 'crocs', value: 5 },
+        { product: 'havaiana com prego', value: 1000 },
+      ],
+    },
+    {
+      category: 'eletros',
+      products: [
+        { product: 'Samsung TV', value: 680 },
+        { product: 'Samsung galaxy', value: 1170 },
+        { product: 'PlayStation 5 super slim', value: 820 },
+        { product: 'Pulsera galaxy watch', value: 570 },
+        { product: 'Iphone 14s Pro max', value: 760 },
+        { product: 'Ar-condicionado', value: 500 },
+      ],
+    },
+    {
+      category: 'cozinha',
+      products: [
+        { product: 'Conjunto facas', value: 180 },
+        { product: 'Microondas', value: 478 },
+        { product: 'air fryer', value: 1170 },
+        { product: 'Liquidificador', value: 820 },
+        { product: 'Espremedor de laranja', value: 570 },
+        { product: 'Botijão de gás', value: 760 },
+        { product: 'Frigideira antiaderente', value: 478 },
+        { product: 'Escorredor', value: 900 },
+        { product: 'Depurador de ar', value: 500 },
+      ],
+    },
+    {
+      category: 'pets',
+      products: [
+        { product: 'Coleira', value: 180 },
+        { product: 'Ração', value: 478 },
+        { product: 'Bolinha', value: 1170 },
+        { product: 'Mordedor', value: 820 },
+        { product: 'Arranhador', value: 570 },
+        { product: 'Gaiola', value: 760 },
+        { product: 'Casinha', value: 478 },
+        { product: 'Comedouro', value: 978 },
+      ],
+    },
+    {
+      category: 'praia',
+      products: [
+        { product: 'Chapéu', value: 480 },
+        { product: 'Protetor solar', value: 468 },
+        { product: 'Bronzeador', value: 770 },
+        { product: 'Boia', value: 420 },
+        { product: 'Guarda-sol', value: 100 },
+        { product: 'Caiaque', value: 60 },
+        { product: 'Canga', value: 678 },
+        { product: 'Óculos de sol', value: 930 },
+        { product: 'Biquini', value: 1250 },
+      ],
+    },
   ];
 
   const areaChartData = [30, 24, 22, 19, 13, 9, 11, 14, 17, 22, 26, 24, 21, 23, 21,
@@ -119,13 +185,27 @@ export const SalesPage: React.FC = () => {
     setPieYear(true);
     setPieData(pieCharDataYear);
   };
+  const listBarProducts = barChartData[selectedBarCategory].products.map((d, i) => (
+    <tr className="table-line">
+      <td className="table-line-number">{i + 1}</td>
+      <td>{d.product}</td>
+      <td>{d.value}</td>
+    </tr>
+  ));
+
+  const handleBarCategory = (e: ChangeEvent<HTMLSelectElement>) => {
+    setBarCategory(Number(e.target.value));
+  };
+
+  const listBarCategories = barChartData.map((d, i) => (
+    <option value={i}>{d.category}</option>
+  ));
 
   return (
     <Container>
       <Navbar />
       <ElementsContainer>
         <CardsContainer>
-
           <div className="main">
             <div className="top">
               <section className="first">
@@ -133,11 +213,17 @@ export const SalesPage: React.FC = () => {
                   <div className="inside_first">
                     <div className="title">
                       <h3>Setembro 2022</h3>
-                      <button type="button" className="info"> i</button>
+                      <InfoCircleOutlined />
                     </div>
                     <h2>R$ 126,560</h2>
-                    <p> Em relação a Ago/22 12%</p>
-                    <p> Em relação a Set/22 13%</p>
+                    <div className="sale-by-time">
+                      <p> Em relação a Ago/22 12% </p>
+                      <CaretDownOutlined style={{ color: '#8B1A47' }} />
+                    </div>
+                    <div className="sale-by-time">
+                      <p>Em relação a Set/22 13%</p>
+                      <CaretUpOutlined style={{ color: '#8DB580' }} />
+                    </div>
                   </div>
                   <p className="vendas"> Venda diária $R$ 12,423</p>
                 </div>
@@ -145,14 +231,14 @@ export const SalesPage: React.FC = () => {
                 <div className="inside_second">
                   <div className="title">
                     <h3>Saída de produtos diária (últimos 30 dias)</h3>
-                    <button type="button" className="info"> i</button>
+                    <InfoCircleOutlined />
                   </div>
                   <h2>39</h2>
                   <div className="one_graph">
                     <SalesAreaChart data={areaChartData} />
                   </div>
+                  <p className="vendas">Saída de Produtos total (últimos 30 dias) 1,398</p>
                 </div>
-                <p className="vendas">Saída de Produtos total (últimos 30 dias) 1,398</p>
               </section>
               <section className="second">
 
@@ -206,53 +292,22 @@ export const SalesPage: React.FC = () => {
                   <button type="button" className="button_second">Sempre</button>
                   <button type="button" className="button1_second">Este ano</button>
                   <button type="button" className="button2_second">Últimos meses</button>
-                  <select className="select">
-                    <option> Ilhas</option>
-                    <option>No idea</option>
+                  <select onChange={(e) => handleBarCategory(e)} className="select">
+                    {listBarCategories}
                   </select>
                 </div>
               </div>
 
               <div className="third_content">
                 <div className="last_graph">
-                  <SalesBarChart data={barChartData} />
+                  <SalesBarChart data={barChartData[selectedBarCategory].products} />
                 </div>
                 <div className="last_table">
+                  <p className="last-table-list">Produtos mais vendidos</p>
                   <table className="table">
-                    <th>Produtos mais vendidos</th>
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
-
-                    <tr>
-                      <td> Produto N° 1</td>
-                      <td>323,234</td>
-                    </tr>
+                    <tbody>
+                      {listBarProducts}
+                    </tbody>
                   </table>
                 </div>
               </div>
